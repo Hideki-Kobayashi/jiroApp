deb66165586b383f9d783c66ae74208639fa622d
Hideki-Kobayashi <itokoba67@gmail.com>
2017-08-27 20:28:30 +0900

    add_title

---
+++ Jiro_machijikan_united/app/src/main/java/com/lifeistech/android/jiro_machijikan/TitleActivity.java
@@ -0,0 +1,37 @@
+package com.lifeistech.android.jiro_machijikan;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TitleActivity extends AppCompatActivity {
+    Runnable moveToStartActivity;
+    Handler h;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_title);
+
+        //android.app.ActionBar actionBar = getActionBar();
+        //actionBar.hide();
+        getSupportActionBar().hide();
+
+        moveToStartActivity = new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(TitleActivity.this, StartActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+
+        h = new Handler();
+        h.postDelayed(moveToStartActivity, 3000);
+
+
+
+    }
+}
